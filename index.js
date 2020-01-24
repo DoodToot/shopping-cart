6b0002a5e7966f81905ccbe05da0e220e1c7031a
afd <afd@alup5.academiapostal.es.local>
2020-01-24 19:32:58 +0100

    añadir_function_calcularPeso

--- index.js
+++ index.js
@@ -1,21 +1,34 @@
 
-
 /**
  * Cálculo del total de la compra.
  * 
  * @param {array de objetos} carrito 
  */
+
 export function calcularTotal(carrito) {
-  var total = 0;
+  var totalPrecio = 0;
   for (var i = 0; i<carrito.length; i++) {
     if (carrito[i].cantidad) {
-    total += carrito[i].precio * carrito[i].cantidad;
+    totalPrecio += carrito[i].precio * carrito[i].cantidad;
   } else {
-    total += carrito[i].precio;
+    totalPrecio += carrito[i].precio;
   }
   }
-  return total;
+  return totalPrecio;
 }
+
+export function calcularPeso(carrito) {
+  var totalPeso = 0;
+  for (var i = 0; i<carrito.length; i++) {
+    if (carrito[i].cantidad) {
+    totalPeso += carrito[i].peso * carrito[i].cantidad;
+  } else {
+    totalPeso += carrito[i].peso;
+  }
+  }
+  return totalPeso;
+}
+
 // Pan (1) ->         1€
 // Leche (1) ->       1€
 // Huevos (12) ->     0.30€
--- test.js
+++ test.js
@@ -3,7 +3,7 @@ import { equal } from "assert";
 
 describe(('Cálculo del total'), () => {
   it('carrito vacío', () => { equal(calcularTotal([]), 0) });
-  it('comprar pan', () => { equal(calcularTotal([{producto: "Pan", precio: 1, cantidad: 1}]), 1) });
+  it('comprar pan', () => { equal(calcularTotal([{producto: "Pan", precio: 1, cantidad: 1, peso: 0.25}]), 1) });
   it('compar leche', () => { equal(calcularTotal([{producto: "Leche", precio: 1, cantidad: 1}]), 1) });
   it('compar pan y leche', () => { equal(calcularTotal([{producto: "Pan", precio: 1,}, {producto: "Leche", precio: 1, cantidad: 1}]), 2) });
   it('compar huevos', () => { equal(calcularTotal([{producto: "Huevo", precio: 30, cantidad: 12}]), 360) });
