183c0f350449ab14e52535815104172452167d82
afd <afd@alup5.academiapostal.es.local>
2020-01-24 17:55:29 +0100

    función_calcularPrecio

--- index.js
+++ index.js
@@ -6,7 +6,12 @@
  * @param {array de objetos} carrito 
  */
 export function calcularTotal(carrito) {
-  return 0;
+  var total = 0;
+  for (var i = 0; i<carrito.length; i++) {
+    total += carrito[i].precio * carrito[i].cantidad;
+  }
+  return total;
 }
 
-
+// Pan (1) ->     1€
+// Leche (1) ->   1€
--- test.js
+++ test.js
@@ -1,6 +1,8 @@
-import { toRoman } from './index';
+import { calcularTotal } from './index';
 import { equal } from "assert";
 
 describe(('Cálculo del total'), () => {
   it('carrito vacío', () => { equal(calcularTotal([]), 0) })
-})
+  it('comprar pan', () => { equal(calcularTotal([{producto: "Pan", precio: 1, cantidad: 1}]), 1) })
+  it('compar leche', () => { equal(calcularTotal([{producto: "Leche", precio: 1, cantidad: 1}]), 1) })
+ })
