a092f8f626a9003b65b0bb1d27c8f448bb61efbc
afd <afd@alup5.academiapostal.es.local>
2020-01-24 18:33:08 +0100

    añadir_cantidad

--- index.js
+++ index.js
@@ -8,10 +8,14 @@
 export function calcularTotal(carrito) {
   var total = 0;
   for (var i = 0; i<carrito.length; i++) {
+    if (carrito[i].cantidad) {
     total += carrito[i].precio * carrito[i].cantidad;
+  } else {
+    total += carrito[i].precio;
+  }
   }
   return total;
 }
-
-// Pan (1) ->     1€
-// Leche (1) ->   1€
+// Pan (1) ->         1€
+// Leche (1) ->       1€
+// Huevos (12) ->     0.30€
